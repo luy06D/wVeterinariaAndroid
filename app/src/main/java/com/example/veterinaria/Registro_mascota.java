b8c47aea87221fe7fbe9b353b8386745a9a941de
Luyd06 <cusiluis04@gmail.com>
2023-09-26 17:29:21 -0500

    avanze pantalla principal

---
+++ app/src/main/java/com/example/veterinaria/Registro_mascota.java
@@ -0,0 +1,14 @@
+package com.example.veterinaria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro_mascota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_mascota);
+    }
+}
